2d1d87445162788de34899ddae757e89301e41b9
Cleiton Amaral <Cleito_bss@hotmail.com>
2025-07-22 21:40:05 -0300

    feat criando cumpom de desconto

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const axios = require('axios');
+const { Filter } = require('lucide-react');
 const app = express();
 const PORT = 3000;
-app.use(cors())
-
+app.use(cors());
+let cupons = [
+    {
+        nome: "desconta10",
+        valido: true,
+        quantidade: 10,
+        valor: 0.10
+    }
+];
+app.get("/cupomdesconto/:cupons", (req, res) => {
+    let cupon = req.params.cupons;
+    let cuponsfiltrado = cupons.find(x => x.nome.toUpperCase() == cupon)
+    if (cuponsfiltrado) {
+        cuponsfiltrado.valido && cuponsfiltrado.quantidade ? res.send(cuponsfiltrado.valor) : res.status(400).json({ message: "cupon invalido" })
+    }
+}
+)
 
 
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
+
 });
